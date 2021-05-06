b18c77f41be0ffe948d47faefaf3729f4427152c
Naman <namannigam12@gmail.com>
2021-05-06 22:28:42 +0530

    top k frrequent words

--- src/main/java/edu/bit/datastructures/tree/TopKFrequentWords.java
+++ src/main/java/edu/bit/datastructures/tree/TopKFrequentWords.java
@@ -1,2 +1,45 @@
-package edu.bit.datastructures.tree;public class TopKFrequentWords {
+package edu.bit.datastructures.tree;
+
+import java.util.*;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+
+public class TopKFrequentWords {
+    // what would be the range of N and K, is there a possible relation that could be drawn?
+    public List<String> topKFrequent(String[] words, int k) {
+        // group them into a frequency map [runtime: O(N)] [space: O(N)]
+        Map<String, Long> frequencyMap = Arrays.stream(words)
+                .collect(Collectors.groupingBy(Function.identity(),
+                        Collectors.counting()));
+
+        // alternate thoughts:
+        // sort the entries based on the frequency and then by word [worst case: this would be O(NlogN)]
+        // limit the result to K
+        // improvements, while sorting if I am interested only in first K numbers?
+
+        // create a max heap and ensure only K elements in it,
+        // the insertion in this shall be logK for n elements resulting in O(N*logK)
+
+        // Comparator.<Map.Entry<String, Long>>comparingLong(Map.Entry::getValue)
+        //    .thenComparing(Comparator.<Map.Entry<String, Long>>comparing(e -> e.getKey()).reversed());
+        PriorityQueue<Map.Entry<String, Long>> minHeap = new PriorityQueue<>((o1, o2) ->
+                o1.getValue().equals(o2.getValue()) ?
+                        o2.getKey().compareTo(o1.getKey()) :
+                        o1.getValue().compareTo(o2.getValue()));
+
+
+        for (Map.Entry<String, Long> entry : frequencyMap.entrySet()) {
+            minHeap.offer(entry);
+            if (minHeap.size() > k) {
+                minHeap.poll();
+            }
+        }
+
+        List<String> strings = new ArrayList<>(minHeap.size());
+        while (!minHeap.isEmpty()) {
+            strings.add(minHeap.poll().getKey());
+        }
+        Collections.reverse(strings);
+        return strings;
+    }
 }
--- src/test/java/edu/bit/datastructures/tree/TopKFrequentWordsTest.java
+++ src/test/java/edu/bit/datastructures/tree/TopKFrequentWordsTest.java
@@ -1,4 +1,35 @@
-import static org.junit.jupiter.api.Assertions.*;
+package edu.bit.datastructures.tree;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
 class TopKFrequentWordsTest {
-  
+
+    TopKFrequentWords topKFrequentWords = new TopKFrequentWords();
+
+    @Test
+    void testTopKFrequentElementsWithConflicts() {
+        Assertions.assertEquals(List.of("i", "love"),
+                topKFrequentWords.topKFrequent(
+                        new String[]{"i", "love", "leetcode", "i", "love", "coding"}, 2));
+
+        Assertions.assertEquals(List.of("cinema", "i"),
+                topKFrequentWords.topKFrequent(new String[]{"i", "love", "zee", "cinema"}, 2));
+
+        Assertions.assertEquals(List.of("cinema", "i", "love", "zee"),
+                topKFrequentWords.topKFrequent(new String[]{"i", "love", "zee", "cinema"}, 4));
+    }
+
+    @Test
+    void testTopKFrequentElementsWithoutConflicts() {
+        Assertions.assertEquals(List.of("i", "love"),
+                topKFrequentWords.topKFrequent(
+                        new String[]{"i", "love", "leetcode", "i", "love", "coding", "i"}, 2));
+        Assertions.assertEquals(List.of("the", "is", "sunny", "day"),
+                topKFrequentWords.topKFrequent(
+                        new String[]{"the", "day", "is", "sunny", "the", "the", "the", "sunny", "is", "is"}, 4));
+
+    }
 }
