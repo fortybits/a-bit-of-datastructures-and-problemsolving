5e15bc917731e82eecf5d7d38e4830efe4c5ee4c
Naman <namannigam12@gmail.com>
2021-05-07 23:32:13 +0530

    two sum -- easy

---
+++ src/main/java/edu/bit/datastructures/array/TwoSum.java
@@ -0,0 +1,28 @@
+package edu.bit.datastructures.array;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Given an array of integers nums and an integer target,
+ * return indices of the two numbers such that they add up to target.
+ * <p>
+ * You may assume that each input would have exactly one solution,
+ * and you may not use the same element twice.
+ * <p>
+ * You can return the answer in any order.
+ */
+public class TwoSum {
+
+    public int[] twoSum(int[] nums, int target) {
+        Map<Integer, Integer> indexMap = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+            if (indexMap.containsKey(target - nums[i])) {
+                return new int[]{indexMap.get(target - nums[i]), i};
+            } else {
+                indexMap.put(nums[i], i);
+            }
+        }
+        return new int[]{};
+    }
+}
---
+++ src/test/java/edu/bit/datastructures/array/TwoSumTest.java
@@ -0,0 +1,21 @@
+package edu.bit.datastructures.array;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+class TwoSumTest {
+
+    TwoSum twoSum = new TwoSum();
+
+    @Test
+    void twoSum() {
+        Assertions.assertArrayEquals(new int[]{0, 1},
+                twoSum.twoSum(new int[]{2, 7, 11, 15}, 9));
+
+        Assertions.assertArrayEquals(new int[]{1, 2},
+                twoSum.twoSum(new int[]{3, 2, 4}, 6));
+
+        Assertions.assertArrayEquals(new int[]{0, 1},
+                twoSum.twoSum(new int[]{3, 3}, 6));
+    }
+}
