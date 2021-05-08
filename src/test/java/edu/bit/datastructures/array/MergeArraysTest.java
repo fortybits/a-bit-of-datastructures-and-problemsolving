c62346331d8fc2ad9149725967df8abfe1bce2cd
Naman <namannigam12@gmail.com>
2021-05-08 19:22:57 +0530

    merge arrrays

--- src/test/java/edu/bit/datastructures/array/MergeArraysTest.java
+++ src/test/java/edu/bit/datastructures/array/MergeArraysTest.java
@@ -11,13 +11,13 @@ class MergeArraysTest {
     void merge() {
         int[] a = new int[]{1, 3, 5, 7};
         int[] b = new int[]{0, 2, 6, 8, 9};
-        mergeArrays.merge(a, b, a.length, b.length);
+        mergeArrays.mergeWithoutExtraSpace(a, b, a.length, b.length);
         Assertions.assertArrayEquals(new int[]{0, 1, 2, 3}, a);
         Assertions.assertArrayEquals(new int[]{5, 6, 7, 8, 9}, b);
 
         int[] c = new int[]{10, 12};
         int[] d = new int[]{5, 18, 20};
-        mergeArrays.merge(c, d, c.length, d.length);
+        mergeArrays.mergeWithoutExtraSpace(c, d, c.length, d.length);
         Assertions.assertArrayEquals(new int[]{5, 10}, c);
         Assertions.assertArrayEquals(new int[]{12, 18, 20}, d);
     }
