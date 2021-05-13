edf9ad429c1cb98adbd9c479aadb5426ab15de73
Naman <namannigam12@gmail.com>
2021-05-14 00:18:31 +0530

    lowest common ancestor

--- src/main/java/edu/bit/datastructures/tree/FirstCommonAncestor.java
+++ src/main/java/edu/bit/datastructures/tree/FirstCommonAncestor.java
@@ -9,7 +9,9 @@ public class FirstCommonAncestor {
 
     // with the links to the parents once can traverse back and find the common ancestor node
     public static TreeNode commonAncestor(TreeNode p, TreeNode q) {
-        if (p == q) return p;
+        if (p == q) {
+            return p;
+        }
 
         TreeNode ancestor = p;
         while (ancestor != null) {
@@ -35,25 +37,39 @@ public class FirstCommonAncestor {
     // Checks how many 'special' nodes are located under this root
     public static int covers(TreeNode root, TreeNode p, TreeNode q) {
         int ret = NO_NODES_FOUND;
-        if (root == null) return ret;
-        if (root == p || root == q) ret += 1;
+        if (root == null) {
+            return ret;
+        }
+        if (root == p || root == q) {
+            ret += 1;
+        }
         ret += covers(root.left, p, q);
         if (ret == TWO_NODES_FOUND) // Found p and q
+        {
             return ret;
+        }
         return ret + covers(root.right, p, q);
     }
 
     // this algorithm generally takes O(T) time for T being the length of first ancestor or  in the worst case
     // O(N) time for traversing all N nodes of the tree
     public static TreeNode commonAncestor(TreeNode root, TreeNode p, TreeNode q) {
-        if (q == p && (root.left == q || root.right == q)) return root;
+        if (q == p && (root.left == q || root.right == q)) {
+            return root;
+        }
         int nodesFromLeft = covers(root.left, p, q); // Check left side
         if (nodesFromLeft == TWO_NODES_FOUND) {
-            if (root.left == p || root.left == q) return root.left;
-            else return commonAncestor(root.left, p, q);
+            if (root.left == p || root.left == q) {
+                return root.left;
+            } else {
+                return commonAncestor(root.left, p, q);
+            }
         } else if (nodesFromLeft == ONE_NODE_FOUND) {
-            if (root == p) return p;
-            else if (root == q) return q;
+            if (root == p) {
+                return p;
+            } else if (root == q) {
+                return q;
+            }
         }
 
         int nodesFromRight = covers(root.right, p, q); // Check right side
@@ -113,5 +129,37 @@ public class FirstCommonAncestor {
             return r.node;
         }
         return null;
+    }
+
+
+    // This function returns pointer to LCA of two given
+    // values n1 and n2. This function assumes that n1 and
+    // n2 are present in Binary Tree
+    TreeNode findLowestCommonAncestor(TreeNode node, int n1, int n2) {
+        // Base case
+        if (node == null) {
+            return null;
+        }
+
+        // If either n1 or n2 matches with root's key, report
+        // the presence by returning root (Note that if a key is
+        // ancestor of other, then the ancestor key becomes LCA
+        if (node.val == n1 || node.val == n2) {
+            return node;
+        }
+
+        // Look for keys in left and right subtrees
+        TreeNode left_lca = findLowestCommonAncestor(node.left, n1, n2);
+        TreeNode right_lca = findLowestCommonAncestor(node.right, n1, n2);
+
+        // If both of the above calls return Non-NULL, then one key
+        // is present in once subtree and other is present in other,
+        // So this node is the LCA
+        if (left_lca != null && right_lca != null) {
+            return node;
+        }
+
+        // Otherwise check if left subtree or right subtree is LCA
+        return (left_lca != null) ? left_lca : right_lca;
     }
 }
