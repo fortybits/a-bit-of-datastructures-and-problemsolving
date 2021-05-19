3435af6b6161b021a9130e72ed5ae99290f3c519
Naman <namannigam12@gmail.com>
2021-05-19 23:23:30 +0530

    generate parernthesis - backtracking

---
+++ src/main/java/edu/bit/datastructures/backtrack/GenerateParenthesis.java
@@ -0,0 +1,30 @@
+package edu.bit.datastructures.backtrack;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Given n pairs of parentheses, write a function to generate all combinations of well-formed parentheses.
+ */
+public class GenerateParenthesis {
+
+    public List<String> generateParenthesis(int n) {
+        List<String> list = new ArrayList<>();
+        generateParenthesisHelper("", n, 0, 0, list);
+        return list;
+    }
+
+    void generateParenthesisHelper(String currentPrefix, int max, int open, int close, List<String> matches) {
+        if ((max * 2) == currentPrefix.length()) {
+            matches.add(currentPrefix);
+            return;
+        }
+        if (open < max) {
+            generateParenthesisHelper(currentPrefix + "(", max, open + 1, close, matches);
+        }
+        if (close < open) {
+            generateParenthesisHelper(currentPrefix + ")", max, open, close + 1, matches);
+        }
+    }
+
+}
---
+++ src/test/java/edu/bit/datastructures/backtrack/GenerateParenthesisTest.java
@@ -0,0 +1,20 @@
+package edu.bit.datastructures.backtrack;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+class GenerateParenthesisTest {
+
+    GenerateParenthesis generateParenthesis = new GenerateParenthesis();
+
+
+    @Test
+    void generateParenthesis() {
+        Assertions.assertEquals(List.of("((()))", "(()())", "(())()", "()(())", "()()()"),
+                generateParenthesis.generateParenthesis(3));
+        Assertions.assertEquals(List.of("()"),
+                generateParenthesis.generateParenthesis(1));
+    }
+}
