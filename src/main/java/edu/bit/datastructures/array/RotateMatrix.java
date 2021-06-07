94aef7b519eacf5f0c41c7b3c315b54fcf7e9796
Naman <namannigam12@gmail.com>
2021-06-07 14:27:37 +0530

    rotate matrix simpler

--- src/main/java/edu/bit/datastructures/array/RotateMatrix.java
+++ src/main/java/edu/bit/datastructures/array/RotateMatrix.java
@@ -34,4 +34,19 @@ public class RotateMatrix {
         }
         return true;
     }
+
+    public void rotate(int[][] mat) {
+        for (int i = 0, j = mat.length - 1; i < j; ++i, --j) { // reverse rows order.
+            int[] tmp = mat[i];
+            mat[i] = mat[j];
+            mat[j] = tmp;
+        }
+        for (int r = 0, R = mat.length; r < R; ++r) { // transpose.
+            for (int c = r + 1; c < R; ++c) {
+                int tmp = mat[r][c];
+                mat[r][c] = mat[c][r];
+                mat[c][r] = tmp;
+            }
+        }
+    }
 }
