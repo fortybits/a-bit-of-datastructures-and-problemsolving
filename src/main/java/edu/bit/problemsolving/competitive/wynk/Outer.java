6abf76e175510b4814f964dfb3e1fc4a204af532
Naman <namannigam12@gmail.com>
2021-07-01 21:36:53 +0530

    cleanup

--- src/main/java/edu/bit/problemsolving/competitive/wynk/Outer.java
+++
@@ -1,9 +0,0 @@
-package edu.bit.problemsolving.competitive.wynk;
-
-public class Outer {
-    public static class Inner {
-        public static void foo() {
-            System.out.println("X");
-        }
-    }
-}
