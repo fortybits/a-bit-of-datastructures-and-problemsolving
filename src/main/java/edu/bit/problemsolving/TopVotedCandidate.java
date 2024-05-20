a2ad1680591db7f408a2d034d757f59340af81f1
Naman Nigam <namannigam12@gmail.com>
2024-05-20 08:22:20 +0530

    Query leader at time

---
+++ src/main/java/edu/bit/problemsolving/TopVotedCandidate.java
@@ -0,0 +1,44 @@
+package edu.bit.problemsolving;
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * You are given two integer arrays persons and times. In an election, the ith vote was cast for persons[i] at time times[i].
+ * <p>
+ * For each query at a time t, find the person that was leading the election at time t.
+ * Votes cast at time t will count towards our query. In the case of a tie, the most recent vote (among tied candidates) wins.
+ * <p>
+ * Implement the TopVotedCandidate class:
+ * <p>
+ * TopVotedCandidate(int[] persons, int[] times) Initializes the object with the persons and times arrays.
+ * int q(int t) Returns the number of the person that was leading the election at time t according to the mentioned rules.
+ */
+public class TopVotedCandidate {
+
+    Map<Integer, Integer> lookUpMatrix = new HashMap<>();
+    int[] time;
+
+    public TopVotedCandidate(int[] persons, int[] times) {
+        int n = persons.length;
+        int leader = -1;
+        Map<Integer, Integer> count = new HashMap<>(); // person to vote count
+        time = times;
+        for (int i = 0; i < n; ++i) { // O(N)
+            count.put(persons[i], count.getOrDefault(persons[i], 0) + 1);
+            if (i == 0 || count.get(persons[i]) >= count.get(leader)) {
+                leader = persons[i];
+            }
+            lookUpMatrix.put(times[i], leader);
+        }
+    }
+
+    public int query(int t) {
+        int i = Arrays.binarySearch(time, t); // O(log(N))
+        return i < 0 ? lookUpMatrix.get(time[-i - 2]) : lookUpMatrix.get(time[i]);
+        // The insertion point is defined as the point at which the key would be inserted into the array:
+        // the index of the first element greater than the key,
+        // or a. length if all elements in the array are less than the specified key
+    }
+}
---
+++ src/test/java/edu/bit/problemsolving/TopVotedCandidateTest.java
@@ -0,0 +1,25 @@
+package edu.bit.problemsolving;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class TopVotedCandidateTest {
+
+    TopVotedCandidate topVotedCandidate;
+
+    @Test
+    void queryLeader(){
+        topVotedCandidate = new TopVotedCandidate(new int[]{0, 1, 1, 0, 0, 1, 0},
+                new int[] {0, 5, 10, 15, 20, 25, 30});
+        Assertions.assertEquals(0,topVotedCandidate.query(3));
+        Assertions.assertEquals(1,topVotedCandidate.query(12));
+        Assertions.assertEquals(1,topVotedCandidate.query(25));
+        Assertions.assertEquals(0,topVotedCandidate.query(15));
+        Assertions.assertEquals(0,topVotedCandidate.query(24));
+        Assertions.assertEquals(1,topVotedCandidate.query(8));
+        Assertions.assertEquals(0,topVotedCandidate.query(36)); // boundary test
+    }
+
+}
