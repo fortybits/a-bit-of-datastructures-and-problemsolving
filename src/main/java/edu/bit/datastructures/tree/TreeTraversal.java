67830598c7528ad4d6d55d89e0a7617c38a6f514
Naman Nigam <namannigam12@gmail.com>
2024-07-17 09:05:44 +0530

    Tree traversals

--- src/main/java/edu/bit/datastructures/tree/TreeTraversal.java
+++ src/main/java/edu/bit/datastructures/tree/TreeTraversal.java
@@ -7,25 +7,66 @@ public class TreeTraversal {
 
     // performs an inorder traversal of a tree and stored the vales into a list with runtime O(N)
     // Note: inorder traversal of a BST is considered to be sorted
-    public List<Integer> inorder(TreeNode node) {
-        if (node == null) {
+    public List<Integer> inorder(TreeNode root) {
+        if (root == null) {
             return new ArrayList<>();
         }
 
         List<Integer> values = new ArrayList<>();
-        if (node.left == null && node.right == null) {
-            values.add(node.val);
+        if (root.left == null && root.right == null) {
+            values.add(root.val);
             return values;
         }
 
-        List<Integer> leftNodes = inorder(node.left);
-        int val = node.val;
-        List<Integer> rightNodes = inorder(node.right);
+        List<Integer> leftNodes = inorder(root.left);
+        int val = root.val;
+        List<Integer> rightNodes = inorder(root.right);
 
         values.addAll(leftNodes);
         values.add(val);
         values.addAll(rightNodes);
 
+        return values;
+    }
+
+    public List<Integer> preorder(TreeNode root) {
+        if (root == null) {
+            return new ArrayList<>();
+        }
+        List<Integer> values = new ArrayList<>();
+        if (root.left == null && root.right == null) {
+            values.add(root.val);
+            return values;
+        }
+
+        int val = root.val;
+        List<Integer> leftNodes = preorder(root.left);
+        List<Integer> rightNodes = preorder(root.right);
+
+        values.add(val);
+        values.addAll(leftNodes);
+        values.addAll(rightNodes);
+        return values;
+    }
+
+
+    public List<Integer> postorder(TreeNode root) {
+        if (root == null) {
+            return new ArrayList<>();
+        }
+        List<Integer> values = new ArrayList<>();
+        if (root.left == null && root.right == null) {
+            values.add(root.val);
+            return values;
+        }
+
+        List<Integer> leftNodes = preorder(root.left);
+        List<Integer> rightNodes = preorder(root.right);
+        int val = root.val;
+
+        values.addAll(leftNodes);
+        values.addAll(rightNodes);
+        values.add(val);
         return values;
     }
 }
