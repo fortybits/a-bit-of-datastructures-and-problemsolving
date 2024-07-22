98818629d59a557cd521f3370642b49e2c629768
Naman Nigam <namannigam12@gmail.com>
2024-07-22 09:21:47 +0530

    Right Side View of a Tree

---
+++ src/main/java/edu/bit/datastructures/tree/RightSideView.java
@@ -0,0 +1,43 @@
+package edu.bit.datastructures.tree;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Given the root of a binary tree, imagine yourself standing on the right side of it,
+ * return the values of the nodes you can see ordered from top to bottom.
+ * <p>
+ * Input: root = [1,2,3,null,5,null,4]
+ * Output: [1,3,4]
+ * <p>
+ * Input: root = [1,null,3]
+ * Output: [1,3]
+ * <p>
+ * Input: root = []
+ * Output: []
+ * <p>
+ * Constraints:
+ * The number of nodes in the tree is in the range [0, 100].
+ * -100 <= Node.val <= 100
+ */
+public class RightSideView {
+
+    public List<Integer> rightSideView(TreeNode root) {
+        return rightSideViewHelper(root, new ArrayList<>());
+    }
+
+    List<Integer> rightSideViewHelper(TreeNode root, List<Integer> list) {
+        if (root == null) {
+            return new ArrayList<>();
+        }
+        list.add(root.val);
+        if (root.left == null && root.right == null) {
+            return list;
+        }
+        if (root.right == null) {
+            return rightSideViewHelper(root.left, list);
+        } else {
+            return rightSideViewHelper(root.right, list);
+        }
+    }
+}
