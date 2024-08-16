9eef06660483ffa91469b9aae00d71b596b0e21d
Naman Nigam <namannigam12@gmail.com>
2024-08-16 23:27:12 +0530

    Minimum in rotated array

--- src/main/java/edu/bit/datastructures/search/FindMinimumInRotatedArray.java
+++ src/main/java/edu/bit/datastructures/search/FindMinimumInRotatedArray.java
@@ -24,11 +24,13 @@ public class FindMinimumInRotatedArray {
         int end = n - 1;
 
         while (start < end) {
-            int mid = start + (end - start) / 2;
-            if (nums[mid] < nums[end]) end = mid - 1;
-            else if (nums[mid] > nums[end]) start = mid + 1;
+            int mid = (start + end) / 2;
+            if (mid > 0 && nums[mid] < nums[mid - 1]) {
+                return nums[mid];
+            }
+            if (nums[start] <= nums[mid] && nums[mid] > nums[end]) start = mid + 1;
+            else end = mid - 1;
         }
         return nums[start];
     }
-
 }
--- src/test/java/edu/bit/datastructures/search/FindMinimumInRotatedArrayTest.java
+++ src/test/java/edu/bit/datastructures/search/FindMinimumInRotatedArrayTest.java
@@ -11,5 +11,6 @@ class FindMinimumInRotatedArrayTest {
     void testFindMinimumInRotatedArray() {
         Assertions.assertEquals(0, findMinimumInRotatedArray.findMin(new int[]{4, 5, 6, 7, 0, 1, 2}));
         Assertions.assertEquals(11, findMinimumInRotatedArray.findMin(new int[]{11, 13, 15, 17}));
+        Assertions.assertEquals(1, findMinimumInRotatedArray.findMin(new int[]{3, 1, 2}));
     }
 }
