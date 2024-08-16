92e5bdeef9a6c4b4ac9c541d20cb6f6c11e931b0
Naman Nigam <namannigam12@gmail.com>
2024-08-16 22:53:12 +0530

    Binary search basic

--- src/main/java/edu/bit/datastructures/search/BinarySearch.java
+++ src/main/java/edu/bit/datastructures/search/BinarySearch.java
@@ -1,2 +1,21 @@
-package edu.bit.datastructures.search;public class BinarySearch {
+package edu.bit.datastructures.search;
+
+public class BinarySearch {
+    public int search(int[] array, int target) {
+        int n = array.length;
+        int low = 0;
+        int high = n - 1;
+
+        while (low <= high) {
+            int mid = (low + high) / 2; // this may cause integer overflow
+            if (array[mid] == target)
+                return mid;
+            else if (array[mid] < target)
+                low = mid + 1;
+            else
+                high = mid - 1;
+        }
+
+        return -1;
+    }
 }
--- src/main/java/edu/bit/datastructures/search/FindPeakElements.java
+++ src/main/java/edu/bit/datastructures/search/FindPeakElements.java
@@ -1,2 +1,20 @@
-package edu.bit.datastructures.search;public class FindPeakElements {
+package edu.bit.datastructures.search;
+
+/**
+ * A peak element is an element that is strictly greater than its neighbors.
+ * <p>
+ * Given a 0-indexed integer array nums, find a peak element, and return its index.
+ * If the array contains multiple peaks, return the index to any of the peaks.
+ * <p>
+ * You may imagine that nums[-1] = nums[n] = -∞. In other words, an element is always considered to be strictly greater
+ * than a neighbor that is outside the array.
+ * <p>
+ * You must write an algorithm that runs in O(log n) time.
+ */
+public class FindPeakElements {
+
+    public int findPeakElement(int[] nums) {
+
+    }
+
 }
