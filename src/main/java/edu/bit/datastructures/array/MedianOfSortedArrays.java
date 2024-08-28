313dd46d05d635ed449d3067843632efa00d62d2
Naman Nigam <namannigam12@gmail.com>
2024-08-28 07:58:29 +0530

    Hard - Median of sorted arrays.

--- src/main/java/edu/bit/datastructures/array/MedianOfSortedArrays.java
+++ src/main/java/edu/bit/datastructures/array/MedianOfSortedArrays.java
@@ -53,4 +53,29 @@ public class MedianOfSortedArrays {
 
         return secondMedian;
     }
+
+    double findMedianSortedArraysUsingBinarySearch(int[] first, int[] second) {
+        int n = first.length;
+        int m = second.length;
+        if (n < m) {
+            return findMedianSortedArraysUsingBinarySearch(second, first);    // Make sure second array is shorter
+        }
+
+        int lo = 0;
+        int hi = m * 2;
+        while (lo <= hi) {
+            int mid2 = (lo + hi) / 2;   // Try Cut 2
+            int mid1 = n + m - mid2;  // Calculate Cut 1 accordingly
+
+            double L1 = (mid1 == 0) ? Integer.MIN_VALUE : first[(mid1 - 1) / 2];    // Get L1, R1, L2, R2 respectively
+            double L2 = (mid2 == 0) ? Integer.MIN_VALUE : second[(mid2 - 1) / 2];
+            double R1 = (mid1 == n * 2) ? Integer.MAX_VALUE : first[(mid1) / 2];
+            double R2 = (mid2 == m * 2) ? Integer.MAX_VALUE : second[(mid2) / 2];
+
+            if (L1 > R2) lo = mid2 + 1;        // A1's lower half is too big; need to move C1 left (C2 right)
+            else if (L2 > R1) hi = mid2 - 1;    // A2's lower half too big; need to move C2 left.
+            else return (Math.max(L1, L2) + Math.min(R1, R2)) / 2;    // Otherwise, that's the right cut.
+        }
+        return -1;
+    }
 }
