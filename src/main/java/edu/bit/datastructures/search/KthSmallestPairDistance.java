19d2ece49433a3d83483db8de0a7af2164e8213e
Naman Nigam <namannigam12@gmail.com>
2024-09-07 23:17:07 +0530

    Binary search algorithm

---
+++ src/main/java/edu/bit/datastructures/search/KthSmallestPairDistance.java
@@ -0,0 +1,19 @@
+package edu.bit.datastructures.search;
+
+/**
+ * The distance of a pair of integers a and b is defined as the absolute difference between a and b.
+ * <p>
+ * Given an integer array nums and an integer k, return the kth smallest distance among
+ * all the pairs nums[i] and nums[j] where 0 <= i < j < nums.length.
+ * <p>
+ * n == nums.length
+ * 2 <= n <= 104
+ * 0 <= nums[i] <= 106
+ * 1 <= k <= n * (n - 1) / 2
+ */
+public class KthSmallestPairDistance {
+
+    public int smallestDistancePair(int[] nums, int k) {
+        return 0;
+    }
+}
--- src/test/java/edu/bit/datastructures/graph/ShortestPathWithObstaclesTest.java
+++ src/test/java/edu/bit/datastructures/graph/ShortestPathWithObstaclesTest.java
@@ -1,4 +1,18 @@
-import static org.junit.jupiter.api.Assertions.*;
+package edu.bit.datastructures.graph;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
 class ShortestPathWithObstaclesTest {
-  
+
+    ShortestPathWithObstacles shortestPathWithObstacles = new ShortestPathWithObstacles();
+
+    @Test
+    void testShortestPath() {
+        Assertions.assertEquals(6,
+                shortestPathWithObstacles.shortestPath(new int[][]{{0, 0, 0}, {1, 1, 0}, {0, 0, 0}, {0, 1, 1}, {0, 0, 0}}, 1));
+        Assertions.assertEquals(-1,
+                shortestPathWithObstacles.shortestPath(new int[][]{{0, 1, 1}, {1, 1, 1}, {1, 0, 0}}, 1));
+    }
+
 }
