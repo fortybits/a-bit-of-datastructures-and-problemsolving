9e27e2397c5d7c15fff784023368f3f5febde3b1
Naman Nigam <namannigam12@gmail.com>
2024-09-09 08:36:45 +0530

    Find kth smallest pair dist

--- src/main/java/edu/bit/datastructures/search/KthSmallestPairDistance.java
+++ src/main/java/edu/bit/datastructures/search/KthSmallestPairDistance.java
@@ -1,5 +1,7 @@
 package edu.bit.datastructures.search;
 
+import java.util.Arrays;
+
 /**
  * The distance of a pair of integers a and b is defined as the absolute difference between a and b.
  * <p>
@@ -14,6 +16,50 @@ package edu.bit.datastructures.search;
 public class KthSmallestPairDistance {
 
     public int smallestDistancePair(int[] nums, int k) {
-        return 0;
+        int n = nums.length;
+        Arrays.sort(nums); // nlogn
+
+        // Minimum absolute difference
+        int low = nums[1] - nums[0];
+        for (int i = 1; i < n - 1; i++)
+            low = Math.min(low, nums[i + 1] - nums[i]);
+
+        // Maximum absolute difference
+        int high = nums[n - 1] - nums[0];
+
+        // Do binary search for k-th absolute difference
+        while (low < high) {
+            int mid = low + (high - low) / 2;
+            if (countPairs(nums, mid) < k)
+                low = mid + 1;
+            else
+                high = mid;
+        }
+
+        return low;
+    }
+
+    // Returns number of pairs with absolute difference less than or equal to mid.
+    private int countPairs(int[] nums, int mid) {
+        int n = nums.length;
+        int res = 0;
+        for (int i = 0; i < n; i++) {
+            res += upperBound(nums, i, n - 1, nums[i] + mid) - i - 1;
+        }
+        return res;
+    }
+
+    // Returns index of first index of element which is greater than key
+    private int upperBound(int[] nums, int low, int high, int key) {
+        if (nums[high] <= key) return high + 1;
+        while (low < high) {
+            int mid = low + (high - low) / 2;
+            if (key >= nums[mid]) {
+                low = mid + 1;
+            } else {
+                high = mid;
+            }
+        }
+        return low;
     }
 }
