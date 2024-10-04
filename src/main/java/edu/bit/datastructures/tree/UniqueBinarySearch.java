7e6906d9a643416cf03a10a87db40ff88293a758
Naman Nigam <namannigam12@gmail.com>
2024-10-04 23:05:52 +0530

    Unique Binary Search - DP

--- src/main/java/edu/bit/datastructures/tree/UniqueBinarySearch.java
+++ src/main/java/edu/bit/datastructures/tree/UniqueBinarySearch.java
@@ -1,6 +1,6 @@
 package edu.bit.datastructures.tree;
 
-import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.List;
 
 /**
@@ -11,6 +11,40 @@ public class UniqueBinarySearch {
 
     // approach: treesN = "N" + treesN(n-1)
     public List<TreeNode> generateTrees(int n) {
-        return new ArrayList<>();
+        return generateSubtrees(1, n);
+    }
+
+    private List<TreeNode> generateSubtrees(int s, int e) {
+        List<TreeNode> res = new LinkedList<>();
+        if (s > e) {
+            res.add(null); // empty tree
+            return res;
+        }
+
+        for (int i = s; i <= e; ++i) {
+            List<TreeNode> leftSubtrees = generateSubtrees(s, i - 1);
+            List<TreeNode> rightSubtrees = generateSubtrees(i + 1, e);
+
+            for (TreeNode left : leftSubtrees) {
+                for (TreeNode right : rightSubtrees) {
+                    TreeNode root = new TreeNode(i);
+                    root.left = left;
+                    root.right = right;
+                    res.add(root);
+                }
+            }
+        }
+        return res;
+    }
+
+    // how about returning the count of such trees
+    public int numTrees(int n) {
+        int[] dp = new int[n + 1];
+        dp[0] = 1;
+        dp[1] = 1;
+        for (int level = 2; level <= n; level++)
+            for (int root = 1; root <= level; root++)
+                dp[level] += dp[level - root] * dp[root - 1];
+        return dp[n];
     }
 }
