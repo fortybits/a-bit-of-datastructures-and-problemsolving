433c89d83858f053274c43648420a58393b05088
Naman Nigam <namannigam12@gmail.com>
2024-10-10 07:56:14 +0530

    Intersecting elements alternative

--- src/main/java/edu/bit/datastructures/hash/IntersectionUnionOfArrays.java
+++ src/main/java/edu/bit/datastructures/hash/IntersectionUnionOfArrays.java
@@ -1,23 +1,37 @@
 package edu.bit.datastructures.hash;
 
+import java.util.Arrays;
 import java.util.HashSet;
+import java.util.List;
 import java.util.Set;
+import java.util.stream.Collectors;
 
 public class IntersectionUnionOfArrays {
 
-    public Set<Integer> intersection(int[] nums1, int[] nums2) {
-        Set<Integer> existingElements = new HashSet<>();
-        Set<Integer> intersectionElements = new HashSet<>();
-        for (int i1 : nums1) {
-            existingElements.add(i1);
-        }
-        for (int i1 : nums2) {
-            if (existingElements.contains(i1)) {
-                intersectionElements.add(i1);
-            }
-        }
-        return intersectionElements;
+    public List<Integer> intersection(int[] nums1, int[] nums2) {
+        Set<Integer> existingElements = Arrays.stream(nums1)
+                .boxed()
+                .collect(Collectors.toSet());
+        return Arrays.stream(nums2)
+                .filter(existingElements::contains)
+                .boxed()
+                .toList(); // preserving the order
     }
+
+    public static String findIntersection(String[] strArr) {
+        Set<Integer> existingElements = Arrays.stream(strArr[0].split(","))
+                .map(String::trim)
+                .map(Integer::valueOf)
+                .collect(Collectors.toSet());
+
+        return Arrays.stream(strArr[1].split(","))
+                .map(String::trim)
+                .map(Integer::valueOf)
+                .filter(existingElements::contains)
+                .map(String::valueOf)
+                .collect(Collectors.joining(","));
+    }
+
 
     public Set<Integer> union(int[] nums1, int[] nums2) {
         Set<Integer> unionElements = new HashSet<>();
